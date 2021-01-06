f2e23f08735be6a05d38764adaf710595cfec8a6
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-01-06 11:47:47 -0800

    basic req added

---
+++ app.js
@@ -0,0 +1,34 @@
+// document.querySelector("#deduct").addEventListener('click', (e)=> convert(e))
+
+// document.querySelector("#add").addEventListener('click', (e)=> convert(e))
+
+// let output = document.getElementById("output")
+
+// let inputNum = document.getElementById("inputNum")
+
+// function convert(e) {
+//     outputNum = inputNum.value
+//     if (document.getElementById("deduct").click) {
+//         newNum = inputNum - 1
+//     output.innerText = newNum
+// }
+
+// function convert(e) {
+//     inputNum = inputNum.value
+//     if (document.getElementById("add").click {
+//         newNum = ("output" + 1)
+//     output.innerText = newNum
+// }
+
+
+let display = 0;
+
+document.getElementById('output').innerHTML = display;
+
+function addBtn() {
+  document.getElementById('output').innerHTML = ++display;
+}
+
+function decBtn() {
+  document.getElementById('output').innerHTML = --display;
+}
